38627480050730dc1c1fff65264603a176b1f21d
Alexandrebeau <alexandre.beau@isen.yncrea.fr>
2021-01-05 10:05:19 +0100

    su

--- src/main/java/fr/yncrea/cir3/Othello/domain/GameStatus.java
+++
@@ -1,5 +0,0 @@
-package fr.yncrea.cir3.othello.domain;
-
-public enum GameStatus {
-    STARTED, DRAW, FINISHED
-}
--- src/main/java/fr/yncrea/cir3/Othello/repository/GameRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.yncrea.cir3.othello.repository;
-
-import fr.yncrea.cir3.othello.domain.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GameRepository extends JpaRepository<Game, Long> {
-}
